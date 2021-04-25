e73282d2b862f18cf25b4ba1a49a0d10c856ff52
Anastasia Dubkovetskaya <k1tanas@yandex.ru>
2021-04-25 15:17:17 +0200

    исправлены ошибки

--- script/index.js
+++ script/index.js
@@ -2,13 +2,11 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const profileEditButton = document.querySelector('.profile__edit-button');
 
-const hearts = document.querySelectorAll('.card__like');
-
 const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__form-button_close');
+const closePopupButton = document.querySelector('.popup__form-button-close');
 const popupForm = document.querySelector('.popup__form');
-const inputName = document.querySelector('.popup__form-input_name');
-const inputAbout = document.querySelector('.popup__form-input_about');
+const inputName = document.querySelector('.popup__form-input[name=name]');
+const inputAbout = document.querySelector('.popup__form-input[name=about]');
 
 function showPopup() {
   inputName.value = profileTitle.textContent;
@@ -23,15 +21,10 @@ function closePopup() {
 function handleSubmit(e) {
   e.preventDefault();
 
-  const formData = new FormData(popupForm);
-  profileTitle.textContent = formData.get('input_name');
-  profileSubtitle.textContent = formData.get('input_about');
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputAbout.value;
   popupForm.reset();
   closePopup();
-}
-
-function handleLikeClick(e) {
-  e.target.classList.toggle('card__like_active');
 }
 
 profileEditButton.addEventListener('click', showPopup);
@@ -39,7 +32,3 @@ profileEditButton.addEventListener('click', showPopup);
 closePopupButton.addEventListener('click', closePopup);
 
 popupForm.addEventListener('submit', handleSubmit);
-
-for (let i = 0; i < hearts.length; i++) {
-  hearts[i].addEventListener('click', handleLikeClick);
-}
